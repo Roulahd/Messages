542cf191967d749ff24476f8d07b980b876c2101
Roula Haddad <Roula.Haddad@wdc.com>
2020-03-13 14:30:09 +0200

    added documentation

--- infra/message_manager.py
+++ infra/message_manager.py
@@ -1,5 +1,6 @@
 from utils import Utils
 import os
+import pymongo
 
 
 class MessageManager(object):
@@ -11,7 +12,7 @@ class MessageManager(object):
     def write_msg(msg):
         MessageManager._wait_while_locked()
         # Remove text file only if message manager just started
-        MessageManager._remove_text_file()
+        MessageManager._clean_data_base()
         file_name = Utils.get_params()['TextFile']
 
         with open(file_name, 'a') as db:
@@ -32,11 +33,13 @@ class MessageManager(object):
 
         index_msg_found = False
         while not index_msg_found:
+            MessageManager.lock = True
             db = open(file_name, "r")
             lines = db.read().split('\n')
             db.close()
             if len(lines) > index + 1:
                 index_msg_found = True
+            MessageManager.lock = False
 
         db = open(file_name, "r")
         result = db.read().split('\n')[index]
@@ -46,11 +49,20 @@ class MessageManager(object):
         return result
 
     @staticmethod
-    def _remove_text_file():
-        file_name = Utils.get_params()['TextFile']
-        if os.path.exists(file_name) and MessageManager.started:
-            os.remove(file_name)
-            MessageManager.started = False
+    def _clean_data_base():
+        """
+        Supporting both Text file or MongoDB
+        :return:
+        """
+        run_on = Utils.get_params()['RunOn']
+        if run_on == 'TF':
+            file_name = Utils.get_params()['TextFile']
+            if os.path.exists(file_name) and MessageManager.started:
+                os.remove(file_name)
+        else:
+            my_col = MessageManager._get_collection()
+            my_col.delete({})
+        MessageManager.started = False
 
     @staticmethod
     def _wait_while_locked():
@@ -58,3 +70,13 @@ class MessageManager(object):
             if not MessageManager.lock:
                 MessageManager.lock = True
                 break
+
+    """
+    Mongo DB Helper functions
+    """
+    @staticmethod
+    def _get_collection():
+        mongodb_client = pymongo.MongoClient("mongodb://localhost:27017/")
+        my_db = mongodb_client["MyDatabase"]
+        my_col = my_db["Messages"]
+        return my_col
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from infra.read_write_process import ReadWriteProcess, Action
+import pymongo
+from infra.read_write_process import ReadWriteProcess,Action
+import random, string
 
 
 def main():
-    wp = ReadWriteProcess(action=Action.Write, messages_number=100)
-    rp = ReadWriteProcess(action=Action.Read, messages_number=100)
+    messages = []
+    s = string.lowercase + string.uppercase + string.digits
+    for _ in range(2000):
+        messages.append(''.join(random.sample(s, 10)))
+    wp = ReadWriteProcess(action=Action.Write, messages_number=2000, messages=messages)
     wp.run()
-    rp.run()
     wp.wait_for_other()
-    rp.wait_for_other()
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["messages"]
+    mycol.delete_many({})
+    # for i in range(3):
+    #     my_post = {"id": str(i+1), "Message": "Connect Successfully"}
+    #     mycol.insert_one(my_post)
 
+    messages = mycol.find()
+
+    for message in messages:
+        print(message)
+        print(type(message))
 
 if __name__ == "__main__":
     main()
