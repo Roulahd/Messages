ce0ced0582915a2fcf85e3a5f6074aeb7a037c91
Roula Haddad <Roula.Haddad@wdc.com>
2020-03-14 13:02:50 +0200

    fixed comments

--- infra/message_manager.py
+++ infra/message_manager.py
@@ -9,6 +9,12 @@ class MessageManager(object):
 
     @staticmethod
     def write_msg(msg):
+        """
+        This method writes given or default message on TF or MongoDB
+        Will wait for: lock to be false - Because we don't want two processes using the TF or MongoDB in the same time
+        :param msg: message to write
+        :return:
+        """
         MessageManager._wait_while_locked()
         # Remove text file only if message manager just started
         MessageManager._remove_text_file()
@@ -21,6 +27,15 @@ class MessageManager(object):
 
     @staticmethod
     def read_msg(index):
+        """
+        This method reads message given a specific index in regards of TF or MongoDB
+        Will wait for:
+        1- lock to be false - Because we don't want two processes using the TF or MongoDB in the same time
+        2- Wait for the entry in the specific index to be written in order to read it,
+           Because we may have some cases the read process is ahead of the write process
+        :param index: where to read
+        :return: return read message
+        """
         file_name = Utils.get_params()['TextFile']
         # While File doesn't exist, it will run only once, when Write create the file first time
         while True:
@@ -47,6 +62,10 @@ class MessageManager(object):
 
     @staticmethod
     def _remove_text_file():
+        """
+        This method deletes TF only on the beginning of program's life
+        :return:
+        """
         file_name = Utils.get_params()['TextFile']
         if os.path.exists(file_name) and MessageManager.started:
             os.remove(file_name)
@@ -54,6 +73,10 @@ class MessageManager(object):
 
     @staticmethod
     def _wait_while_locked():
+        """
+        Current process waits for the lock to be released, once released it will be locked again by the current process
+        :return:
+        """
         while True:
             if not MessageManager.lock:
                 MessageManager.lock = True
